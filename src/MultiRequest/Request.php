a352405efa0777b707f89b97f493cdeccc46848d
Justinas Bolys <justinas.bolys@gmail.com>
2015-03-08 10:32:43 +0200

    Removed bugfix for PHP curl headers
    Not needed as it was libcurl bug, also it's working fine without fix on my WAMP with PHP 5.6.

--- src/MultiRequest/Request.php
+++ src/MultiRequest/Request.php
@@ -207,25 +207,14 @@ class Request {
 		$this->error = curl_error($curlHandle);
 		$responseData = curl_multi_getcontent($curlHandle);
 
-		// fix bug? https://bugs.php.net/bug.php?id=63894
-		preg_match_all('/.*Content-Length: (\d+).*/mi', $responseData, $matches);
-
-		$contentLength = array_pop($matches[1]);
-
 		// HTTP/1.0 200 Connection established\r\nProxy-agent: Kerio WinRoute Firewall/6.2.2 build 1746\r\n\r\nHTTP
 		if(stripos($responseData, "HTTP/1.0 200 Connection established\r\n\r\n") !== false) {
 			$responseData = str_ireplace("HTTP/1.0 200 Connection established\r\n\r\n", '', $responseData);
 		}
 
-		if (is_null($contentLength) || $contentLength == 0) {
-			$this->responseHeaders = mb_substr($responseData, 0, curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE));
-			$this->responseContent = mb_substr($responseData, curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE));
-			
-		} else {
-			$this->responseHeaders = mb_substr($responseData, 0, mb_strlen($responseData) - $contentLength);
-			$this->responseContent = mb_substr($responseData, mb_strlen($responseData) - $contentLength);
-		}
-
+		$this->responseHeaders = mb_substr($responseData, 0, curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE));
+		$this->responseContent = mb_substr($responseData, curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE));
+		
 		$clientEncoding = $this->detectClientCharset($this->getResponseHeaders());
 		if($clientEncoding && $clientEncoding != $this->serverEncoding) {
 			self::$clientsEncodings[$this->getDomain()] = $clientEncoding;
